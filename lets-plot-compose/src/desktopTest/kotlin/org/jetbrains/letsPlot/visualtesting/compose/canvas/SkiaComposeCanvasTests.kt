bc4dc165284df1a2c4e413e98e7ca557f3fb9eec
Ivan Kupriyanov <horis.ikupriyanov@gmail.com>
2026-03-04 13:54:09 +0100

    Add SkiaComposeCanvasTests to validate canvas rendering functionality

---
+++ lets-plot-compose/src/desktopTest/kotlin/org/jetbrains/letsPlot/visualtesting/compose/canvas/SkiaComposeCanvasTests.kt
@@ -0,0 +1,23 @@
+package org.jetbrains.letsPlot.visualtesting.compose.canvas
+
+import org.jetbrains.letsPlot.compose.canvas.SkiaCanvasPeer
+import org.jetbrains.letsPlot.visualtesting.AwtBitmapIO
+import org.jetbrains.letsPlot.visualtesting.ImageComparer
+import org.jetbrains.letsPlot.visualtesting.canvas.AllCanvasTests
+import kotlin.test.Test
+
+
+class SkiaComposeCanvasTests {
+
+    @Test
+    fun runAllCanvasTests() {
+        val awtBitmapIO = AwtBitmapIO(
+            expectedImagesDir = "/src/desktopTest/resources/expected-images",
+            subdir = "/canvas"
+        )
+        val canvasPeer = SkiaCanvasPeer()
+        val imageComparer = ImageComparer(canvasPeer, awtBitmapIO, silent = true)
+
+        AllCanvasTests.runAllTests(canvasPeer, imageComparer)
+    }
+}
