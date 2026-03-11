9971fd88b4042c38593cdf84a67d4b09a3f0ca6e
Ivan Kupriyanov <horis.ikupriyanov@gmail.com>
2026-03-11 13:37:07 +0100

    Add macOS-specific image baselines due to text rendering differences

--- lets-plot-compose/src/desktopTest/kotlin/org/jetbrains/letsPlot/visualtesting/compose/canvas/SkiaComposeCanvasTests.kt
+++ lets-plot-compose/src/desktopTest/kotlin/org/jetbrains/letsPlot/visualtesting/compose/canvas/SkiaComposeCanvasTests.kt
@@ -9,15 +9,19 @@ import kotlin.test.Test
 
 
 class SkiaComposeCanvasTests {
-
     @Test
     fun runAllCanvasTests() {
+        val os = System.getProperty("os.name").lowercase()
+        val platf = when {
+            os.contains("mac") -> "/macos" // Font thickness differs on macOS, so we use separate expected images.
+            else -> ""
+        }
+
         val awtBitmapIO = AwtBitmapIO(
-            expectedImagesDir = "/src/desktopTest/resources/expected-images",
+            expectedImagesDir = "/src/desktopTest/resources/expected-images$platf",
             subdir = "/canvas"
         )
-        val fontManager = NotoFontManager.INSTANCE
-        val canvasPeer = SkiaCanvasPeer(fontManager)
+        val canvasPeer = SkiaCanvasPeer(NotoFontManager.INSTANCE)
         val imageComparer = ImageComparer(canvasPeer, awtBitmapIO, silent = true)
 
         AllCanvasTests.runAllTests(canvasPeer, imageComparer)
