f583fdaf6960688c1eb6790baa3ef47dc90b21e9
Ivan Kupriyanov <horis.ikupriyanov@gmail.com>
2026-04-02 19:23:42 +0200

    Cleanup: Delete macOS image baselines in favor of ComparisonProfile adjustments

--- lets-plot-compose/src/desktopTest/kotlin/org/jetbrains/letsPlot/visualtesting/compose/canvas/SkiaComposeCanvasTests.kt
+++ lets-plot-compose/src/desktopTest/kotlin/org/jetbrains/letsPlot/visualtesting/compose/canvas/SkiaComposeCanvasTests.kt
@@ -11,14 +11,8 @@ import kotlin.test.Test
 class SkiaComposeCanvasTests {
     @Test
     fun runAllCanvasTests() {
-        val os = System.getProperty("os.name").lowercase()
-        val platf = when {
-            os.contains("mac") -> "/macos" // Font thickness differs on macOS, so we use separate expected images.
-            else -> ""
-        }
-
         val awtBitmapIO = AwtBitmapIO(
-            expectedImagesDir = "/src/desktopTest/resources/expected-images$platf",
+            expectedImagesDir = "/src/desktopTest/resources/expected-images",
             subdir = "/canvas"
         )
         val canvasPeer = SkiaCanvasPeer(NotoFontManager.INSTANCE)
